0405d1de43c6af8a6c43b8b46b741fbd94d7d665
sheemabraza <sheemabraza42@gmail.com>
2024-09-26 23:49:51 +0530

    Updated the project with new changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "4471e878f8694991b867a5b0aa68388a";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
